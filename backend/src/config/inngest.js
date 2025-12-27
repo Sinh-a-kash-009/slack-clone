b8e79bd554e25fc220e4c74078299b5757e80c8c
Akash Sinha <akashsinha0027@gmail.com>
2025-12-27 17:02:30 +0530

    inngest added with some fixes

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,40 @@
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "slack-clone" });
+
+// Create an empty array where we'll export future Inngest functions
+
+const syncUser=inngest.createFunction(
+    {
+        id:'sync-user'
+    },
+    {
+        event:'clerk.user.created',
+    },
+    async ({ event }) => {
+        const { id, email_addresses, image_url, first_name, last_name } = event.data;
+
+        await User.create({
+            clerkId:id,
+            email:email_addresses[0]?.email_address,
+            name:`${first_name || ''} ${last_name || ''}`,
+            imageUrl:image_url || '',
+        })
+    }
+)
+
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
+  }
+);
+
+
+export const functions = [syncUser,deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,35 @@
-import {ENV} from './config/env.js'
+import { ENV } from './config/env.js'
 import express from 'express'
 import { connectDB } from './config/db.js'
-const app=express();
-const PORT=ENV.PORT
+import { clerkMiddleware } from '@clerk/express'
+import { inngest, functions } from './config/inngest.js'
+import { serve } from "inngest/express";
+const app = express();
+const PORT = ENV.PORT
 
-app.get('/',(req,res)=>{
+app.use(express.json()) // parse json data 
+app.use(clerkMiddleware())
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-    connectDB()
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (ENV.NODE_ENV === 'development') {
+            app.listen(PORT, () => {
+                console.log(`Server is running on port ${PORT}`)
+                connectDB()
+            })
+        }
+    } catch (error) {
+        console.error('error staring server :',error)
+        process.exit(1); //Exit the process with a failure code 
+    }
+}
+
+startServer();
+
+
+export default app;
